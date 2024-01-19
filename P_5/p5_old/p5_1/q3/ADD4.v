04e9f278338b84da1ff797c6db711afc664f3ec0
GentleOstrich <273819111@qq.com>
2024-01-20 03:12:18 +0800

    modify

--- P_5/p5_old/p5_1/q3/ADD4.v
+++
@@ -1,9 +0,0 @@
-`include "defines.v"
-module ADD4 (
-    input [31:0] pc,
-    output reg [31:0] pcadd4 
-);
-    always @(*) begin
-        pcadd4 = pc + 4;
-    end
-endmodule
--- P_6/dump.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-java -jar Mars.jar  a db mc CompactDataAtZero dump 0x00003000-0x00030000 HexText P6/code.txt test.asm
-
-pause
